7dcad2164d351c7bdabd8a3ab41310bf8f559e9b
krim45 <krim45@naver.com>
2021-10-08 22:33:39 +0900

    Stack 구현

---
+++ Data Structure/Stack.js
@@ -0,0 +1,26 @@
+class Stack {
+  constructor() {
+    this.storage = {};
+    this.top = 0;
+  }
+
+  size() {
+    return this.top;
+  }
+
+  push(element) {
+    this.storage[this.top] = element;
+    this.top++;
+  }
+
+  pop() {
+    if (this.size() <= 0) {
+      return;
+    }
+    const result = this.storage[this.top - 1];
+    delete this.storage[this.top - 1];
+    this.top--;
+
+    return result;
+  }
+}
