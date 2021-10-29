d66412b6a3bf378094980dc8d3897c66923dbc6a
krim45 <krim45@naver.com>
2021-10-29 09:47:04 +0900

    Update N17_balancedBrackets.js

--- toy/N17_balancedBrackets.js
+++ toy/N17_balancedBrackets.js
@@ -1,3 +1,30 @@
+// refactoring
+const balancedBrackets = function (str) {
+  const stack = [];
+  const brackets = {
+    '(': 'push',
+    '{': 'push',
+    '[': 'push',
+    ')': '(',
+    '}': '{',
+    ']': '['
+  }
+
+  for (let letter of str) {
+    if (brackets[letter] === 'push') {
+      stack.push(letter);
+    }
+    else if (letter in brackets) {
+      const top = stack.pop();
+      if (top !== brackets[letter]) return false;
+    }
+  }
+
+  return stack.length === 0
+};
+
+
+// naive
 const balancedBrackets = function (str) {
   const stack = [];
 
