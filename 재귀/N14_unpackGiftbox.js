7ad9d078518e4218ea7f40714ea70213b8277e74
krim45 <krim45@naver.com>
2021-11-03 15:52:17 +0900

    Create N14_unpackGiftbox.js

---
+++ 재귀/N14_unpackGiftbox.js
@@ -0,0 +1,47 @@
+function unpackGiftbox(giftBox, wish) {
+  for (let gift of giftBox) {
+    if (gift === wish) {
+      return true;
+    } else if (Array.isArray(gift)) {
+      const isGift = unpackGiftbox(gift, wish);
+      if (isGift === true) return true;
+    }
+  }
+
+  return false;
+}
+
+// ref
+// 다른 풀이 방법 1
+function unpackGiftbox(giftBox, wish) {
+  // recursive case
+  let anotherBoxes = [];
+  for (let i = 0; i < giftBox.length; i++) {
+    if (giftBox[i] === wish) {
+      return true;
+    } else if (Array.isArray(giftBox[i])) {
+      anotherBoxes = anotherBoxes.concat(giftBox[i]);
+    }
+  }
+
+  if (anotherBoxes.length > 0) {
+    return unpackGiftbox(anotherBoxes, wish);
+  }
+
+  // base case
+  return false;
+}
+
+// 다른 풀이 방법 2
+function unpackGiftbox(giftBox, wish) {
+  const result = giftBox.reduce((acc, curr) => {
+    if (curr === wish) {
+      return true;
+    } else if (Array.isArray(curr)) {
+      return unpackGiftbox(curr, wish) || acc;
+    } else {
+      return acc;
+    }
+  }, false);
+  return result;
+}
