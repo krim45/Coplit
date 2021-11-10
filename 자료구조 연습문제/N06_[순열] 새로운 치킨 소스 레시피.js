e063ff420bd330e893d1618717827f1219da4e4e
krim45 <krim45@naver.com>
2021-11-10 19:14:54 +0900

    Create N06_[순열] 새로운 치킨 소스 레시피.js

---
+++ 자료구조 연습문제/N06_[순열] 새로운 치킨 소스 레시피.js
@@ -0,0 +1,47 @@
+// 순열 refactoring
+function newChickenRecipe(stuffArr, choiceNum) {
+  const newStuff = stuffArr.filter(stuff => !/0{3}/.test(stuff))
+  if (newStuff.length < choiceNum || newStuff.length === 0) return []
+  const result = [];
+
+  const permutate = (newStuff, bucket ,choiceNum) => {
+    if (choiceNum === 0) {
+      result.push(bucket);
+      return
+    }
+    
+    newStuff.forEach((stuff, idx, arr) => {
+      const fixed = stuff;
+      const arrSlice = [...arr.slice(0, idx), ...arr.slice(idx + 1)];
+      permutate(arrSlice, [...bucket, fixed], choiceNum - 1);
+    })
+  }
+  permutate(newStuff, [], choiceNum)
+  
+  return result
+}
+
+// 순열
+function newChickenRecipe(stuffArr, choiceNum) {
+  const newStuff = stuffArr.filter(stuff => !/0{3}/.test(stuff))
+  if (newStuff.length < choiceNum || newStuff.length === 0) return []
+
+  const permutate = (newStuff, choiceNum) => {
+    if (choiceNum === 1) {
+      return newStuff.map(v => [v]);
+    }
+    const result = [];
+
+    newStuff.forEach((stuff, idx, arr) => {
+      const fixed = stuff;
+      const arrSlice = [...arr.slice(0, idx), ...arr.slice(idx + 1)];
+      const newPermu = permutate(arrSlice, choiceNum - 1);
+      const combine = newPermu.map(permu => [fixed, ...permu]);
+      result.push(...combine);
+    })
+
+    return result
+  }
+  
+  return  permutate(newStuff, choiceNum)
+}
