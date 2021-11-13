6903b620d54b10b9883c0d2c7aabd57e947307de
krim45 <krim45@naver.com>
2021-11-13 17:54:28 +0900

    Update N18_getItemFromTwoSortedArrays.js

--- toy/N18_getItemFromTwoSortedArrays.js
+++ toy/N18_getItemFromTwoSortedArrays.js
@@ -1,3 +1,43 @@
+// reference
+// k/2번째 값을 확인해서 작은 부분의 왼쪽은 필요가 없다.
+const getItemFromTwoSortedArrays = function (arr1, arr2, k) {
+  let leftIdx = 0;
+  let rightIdx = 0;
+
+  while (k > 0) {
+    let cnt = Math.ceil(k / 2);
+    let leftStep = cnt;
+    let rightStep = cnt;
+
+    if (leftIdx === arr1.length) {
+      rightIdx += k;
+      break;
+    }
+
+    if (rightIdx === arr2.length) {
+      leftIdx += k;
+      break;
+    }
+
+    if (leftStep > arr1.length - leftIdx) leftStep = arr1.length - leftIdx;
+    if (rightStep > arr2.length - rightIdx) rightStep = arr2.length - rightIdx;
+
+    if (arr1[leftIdx + leftStep - 1] < arr2[rightIdx + rightStep - 1]){
+      leftIdx += leftStep;
+      k -= leftStep;
+    } else {
+      rightIdx += rightStep;
+      k -= rightStep;
+    }
+  }
+
+  let leftMax = arr1[leftIdx - 1];
+  let rightMax = arr2[rightIdx - 1];
+
+  return Math.max(leftMax, rightMax);
+};
+
+// 이전 풀이 => 완벽하지 않음. 랜덤 케이스에 간헐적으로 틀림
 const getItemFromTwoSortedArrays = function (arr1, arr2, k) {
   if (k === 0) return arr1[0] < arr2[0] ? arr1[0] : arr2[0];
   if (k === 1) return arr1[0] > arr2[0] ? arr1[0] : arr2[0];
