ef78f447c85eae9b0ba73d4e4389121f22dd248e
krim45 <krim45@naver.com>
2021-11-13 19:05:12 +0900

    Update N04_bubbleSort.js

--- toy/N04_bubbleSort.js
+++ toy/N04_bubbleSort.js
@@ -1,21 +1,31 @@
-// 버블 정렬 
+// naive solution
+const bubbleSort = function (arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
+  return arr;
+};
+
+// 버블 정렬 advanced
 const bubbleSort = function (arr) {
   for (let i = arr.length - 1; i > 0; i--) {
     let isSorted = true;
-
     for (let j = 0; j < i; j++) {
       if (arr[j] > arr[j + 1]) {
         let tmp = arr[j];
-
         arr[j] = arr[j + 1];
         arr[j + 1] = tmp;
         isSorted = false;
       }
     }
 
-    if (isSorted === true) {
-      return arr
-    }
+    if (isSorted === true) return arr
   }
   return arr
 };
