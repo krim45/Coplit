3d1e66796192549222ed28a6cc80f6e506990f81
krim45 <krim45@naver.com>
2021-11-13 22:37:21 +0900

    Update N13_insertionSort.js

--- toy/N13_insertionSort.js
+++ toy/N13_insertionSort.js
@@ -1,3 +1,21 @@
+// refactoring
+// 조금 더 삽입 정렬의 원리와 맞게 코드를 수정
+const insertionSort = function (arr, callback = (value) => value) {
+  for (let i = 1; i < arr.length; i++) {
+    const target = arr[i];
+    for (let j = i - 1; j >= 0; j--) {
+      if (callback(arr[j]) > callback(target)) {
+        arr[j + 1] = arr[j];
+        arr[j] = target;
+      } else {
+        break;
+      }
+    }
+  }
+  return arr
+};
+
+// first solution
 const insertionSort = function (arr, callback = (value) => value) {
   for (let i = 1; i < arr.length; i++) {
     for (let j = i; j >= 0; j--) {
