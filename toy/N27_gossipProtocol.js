f00db9a0bd88b208d14aeb366280f92018149cbc
krim45 <krim45@naver.com>
2021-11-15 22:45:39 +0900

    Create N27_gossipProtocol.js

---
+++ toy/N27_gossipProtocol.js
@@ -0,0 +1,37 @@
+const createMatrix = (village) => {
+  const matrix = [];
+  village.forEach((line) => {
+    const row = [];
+    for (let i = 0; i < line.length; i++) row.push(line[i]);
+    matrix.push(row);
+  });
+  return matrix;
+};
+
+const gossipProtocol = function (village, row, col) {
+  const matrix = createMatrix(village);
+  const M = matrix.length;
+  const N = matrix[0].length;
+  const mX = [-1, 1, 0, 0];
+  const mY = [0, 0, -1, 1];
+  let idx = 0;
+  let day = 0;
+
+  const queue = [[row, col, day]];
+
+  while (queue.length > idx) {
+    let  [startY, startX] = queue[idx];
+    day = queue[idx++][2];
+
+    for (let i = 0; i < 4; i++) {
+      const X = startX + mX[i];
+      const Y = startY + mY[i];
+      
+      if (0 <= Y && Y < M && 0 <= X && X < N && matrix[Y][X] === '1') {
+        matrix[Y][X] = 'x';
+        queue.push([Y, X, day + 1])
+      }
+    }
+  }
+  return day
+};
