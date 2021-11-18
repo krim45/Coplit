aad02f914172fac4d5f33a78c7875d20fa94684c
krim45 <krim45@naver.com>
2021-11-18 10:38:08 +0900

    Create N30_heapSort.js

---
+++ toy/N30_heapSort.js
@@ -0,0 +1,69 @@
+// solution 1
+function swap(idx1, idx2, arr) {
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+}
+
+function getParentIdx(idx) {
+  return Math.floor((idx - 1) / 2)
+}
+
+function getLeftChildIdx(idx) {
+  return idx * 2 + 1;
+}
+
+function getRightChildIdx(idx) {
+  return (idx + 1) * 2;
+}
+
+function insert(heap, item) {
+  heap.push(item);
+  let idx = heap.length - 1;
+  let pIdx = getParentIdx(idx);
+
+  while (pIdx >= 0 && heap[pIdx] > heap[idx]) {
+    swap(idx, pIdx, heap);
+    idx = pIdx;
+    pIdx = getParentIdx(idx);
+  }
+  return heap
+}
+
+function removeRoot(heap) {
+  if (heap.length === 1) return heap.pop()
+
+  const minValue = heap[0];
+  heap[0] = heap.pop();
+  let len = heap.length;
+  let idx = 0;
+  let leftIdx = getLeftChildIdx(idx);
+  let rightIdx = getRightChildIdx(idx);
+  let childIdx = leftIdx;
+
+  while (childIdx < len) {
+    if (rightIdx < len) childIdx = heap[leftIdx] > heap[rightIdx] ? rightIdx : leftIdx;
+    if (heap[childIdx] < heap[idx]) {
+      swap(idx, childIdx, heap);
+      idx = childIdx;
+      leftIdx = getLeftChildIdx(idx);
+      rightIdx = getRightChildIdx(idx);
+    } else {
+      break
+    }
+  }
+  return minValue
+}
+
+const binaryHeap = function (arr) {
+  return arr.reduce((heap, item) => {
+    return insert(heap, item);
+  }, []);
+};
+
+const heapSort = function (arr) {
+  let minHeap = binaryHeap(arr);
+  const result = [];
+  while (minHeap.length > 0) {
+    result.push(removeRoot(minHeap));
+  }
+  return result;
+};
