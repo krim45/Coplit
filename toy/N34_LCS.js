018222554335c63f763617137d893f7453c17175
krim45 <krim45@naver.com>
2021-11-25 23:36:13 +0900

    Update N34_LCS.js

--- toy/N34_LCS.js
+++ toy/N34_LCS.js
@@ -1,17 +1,19 @@
 const LCS = function (str1, str2) {
   const M = str1.length;
   const N = str2.length;
-  let head = 0;
-  let max = 0;
+  const dp = new Array(M + 1).fill(0).map(() => new Array(N + 1).fill(0));
 
-  for (let i = 0; i < M; i++) {
-    for (let j = head; j < N; j++) {
-      if (str1[i] === str2[j]) {
-        head = j + 1;
-        max += 1;
-        i++;
+  for (let i = 0; i <= M; i++) {
+    for (let j = 0; j <= N; j++) {
+      if (i === 0 || j === 0) {
+        dp[i][j] = 0
+      } else if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
       }
     }
   }
-  return max
+
+  return dp[M][N]
 };
