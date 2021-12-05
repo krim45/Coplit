bed684c116724965235e6fda2d57abaa20e06505
krim45 <krim45@naver.com>
2021-12-05 19:20:19 +0900

    Update N40_longestPalindrome.js

--- toy/N40_longestPalindrome.js
+++ toy/N40_longestPalindrome.js
@@ -1,6 +1,35 @@
+// refactoring solution O(N*N)
 const longestPalindrome = function (str) {
   const reversed = str.split('').reverse().join('');
   const len = str.length
+  let result = '';
+
+  for (let left = 0; left < len; left++) {
+    let leftIdx = left;
+    let tmp = '';
+
+    for (let right = 0; right < len; right++) {
+      if (str[leftIdx] === reversed[right] && leftIdx < len) {
+        tmp += str[leftIdx];
+        leftIdx++;
+
+        if (result.length < tmp.length) {
+          result = tmp;
+        }
+      } else {
+        leftIdx = left
+        tmp = ''
+      }
+    }
+  }
+
+  return result.length
+};
+
+// naive solution O(N*N)
+const longestPalindrome = function (str) {
+  const reversed = str.split('').reverse().join('');
+  const len = str.length;
   let result = '';
 
   for (let left = 0; left < len; left++) {
@@ -17,7 +46,7 @@ const longestPalindrome = function (str) {
 
         if (result.length < tmp.length) {
           result = tmp;
-        } 
+        }
       }
     }
   }
